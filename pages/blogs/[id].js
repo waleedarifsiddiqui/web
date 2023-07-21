aef19fafd69ce6493492abf3e168b9df55c26dd8
Waleed Arif <waleedarifsiddiqui@gmail.com>
2023-07-21 12:58:31 +0500

    f

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -22,7 +22,7 @@ const SinglePost = () => {
             {/* <br /> */}
             <Title title="Web Development" className='title-bg' />
             <div className='img py'>
-              <img src={post.cover} alt="" width='100%' height='100%' className='round' />
+              <img src="" alt="" width='100%' height='100%' className='round' />
             </div>
             <div className='desc'>
               <TitleSm title='Phasellus at magna - elit tristique lacinia. Integer a justo vitae arcu fermentum consequat.' />
