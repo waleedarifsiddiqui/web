51782b10c19fcff92ff8d166ff8e64ceebfb8c38
Waleed Arif <waleedarifsiddiqui@gmail.com>
2023-07-21 15:54:59 +0500

    l

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -76,7 +76,7 @@ const Footer = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href='https://www.linkedin.com/company/r-tech-qa-pakistan'>
+                  <Link href='https://www.linkedin.com/company/r-tech-qa-pakistan' target="_blank">
                     <AiFillLinkedin size={25} />
                   </Link>
                 </li>
